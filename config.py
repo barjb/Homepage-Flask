7ea86137d181f97416a4f75df5bdd45205dbc186
jacob <j.bartoszek.jb@gmail.com>
2023-04-10 12:32:48 +0200

    fixed typo

--- config.py
+++ config.py
@@ -5,6 +5,6 @@ import os
 
 
 class Config:
-    POSTGRES_URL = f"postgresql://{os.environ.get['POSTGRES_USER']}:{os.environ.get('POSTGRES_PASSWORD')}@{os.environ.get('POSTGRES_HOST')}:{os.environ.get('POSTGRES_PORT')}/{os.environ.get('POSTGRES_DB')}"
+    POSTGRES_URL = f"postgresql://{os.environ.get('POSTGRES_USER')}:{os.environ.get('POSTGRES_PASSWORD')}@{os.environ.get('POSTGRES_HOST')}:{os.environ.get('POSTGRES_PORT')}/{os.environ.get('POSTGRES_DB')}"
     JWT_SECRET_KEY = os.environ.get('JWT_SECRET_KEY')
     APP_SECRET_KEY = os.environ.get('APP_SECRET_KEY')
